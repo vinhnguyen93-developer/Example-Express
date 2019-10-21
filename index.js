03850d8da0466fbd5d41a2cb42bd99c88bfd5f78
VinhCoder <developerthienkim@gmail.com>
2019-10-21 16:35:54 +0700

    commit all file

---
+++ index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+var userRoute = require('./routes/user.route');
+
+var port = 3000;
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+	res.render('index', {
+		name: "Vinh"
+	});
+});
+
+app.use('/users', userRoute);
+
+app.listen(port, function() {
+	console.log("Server listening on port " + port);
+});
+
